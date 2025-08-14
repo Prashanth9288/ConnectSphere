143c8ce5bca67f8336f39c628e0a1588a61dc35c
uppara prashanth <prashanth939288@gmail.com>
2025-08-14 14:58:39 +0530

    coomplete

--- connectSphere/src/components/UserCard.jsx
+++ connectSphere/src/components/UserCard.jsx
@@ -1,15 +1,12 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
-export default function UserCard({ user, following, toggleFollow }) {
-  const isFollowing = following.some((f) => f.id === user.id);
-
+export default function UserCard({ user, isFollowing, toggleFollow }) {
   return (
     <div className="card">
       <h3>{user.name}</h3>
       <p>@{user.username}</p>
       <div className="card-actions">
-        <Link to={/users/${user.id}} className="btn">View</Link>
+        <Link to={`/users/${user.id}`} className="btn">View</Link>
         <button onClick={() => toggleFollow(user)} className="btn follow">
           {isFollowing ? "Unfollow" : "Follow"}
         </button>
